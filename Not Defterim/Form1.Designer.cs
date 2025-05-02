adfc79c8fa8931b2b68a492a2a4fb8bd56dde6ae
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2025-05-02 21:25:16 +0300

    Txt dosya açma işlemi tamamlandı

--- Not Defterim/Form1.Designer.cs
+++ Not Defterim/Form1.Designer.cs
@@ -40,11 +40,18 @@
             kesKopyalaYapıştırToolStripMenuItem = new ToolStripMenuItem();
             tümünüSeçToolStripMenuItem = new ToolStripMenuItem();
             görünümToolStripMenuItem = new ToolStripMenuItem();
-            yardımToolStripMenuItem = new ToolStripMenuItem();
             yazıTipiSeçToolStripMenuItem = new ToolStripMenuItem();
             temaDeğiştirAçıkKoyuToolStripMenuItem = new ToolStripMenuItem();
+            yardımToolStripMenuItem = new ToolStripMenuItem();
             hakkındaUygulamaBilgisiToolStripMenuItem = new ToolStripMenuItem();
+            richTextBox1 = new RichTextBox();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            toolStripStatusLabel3 = new ToolStripStatusLabel();
+            toolStripStatusLabel4 = new ToolStripStatusLabel();
             menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip1
@@ -68,12 +75,14 @@
             yeniYeniSayfaAçToolStripMenuItem.Name = "yeniYeniSayfaAçToolStripMenuItem";
             yeniYeniSayfaAçToolStripMenuItem.Size = new Size(242, 22);
             yeniYeniSayfaAçToolStripMenuItem.Text = "Yeni (Yeni sayfa aç)";
+            yeniYeniSayfaAçToolStripMenuItem.Click += yeniYeniSayfaAçToolStripMenuItem_Click;
             // 
             // açDosyaSeçipİçeriğiniGetirToolStripMenuItem
             // 
             açDosyaSeçipİçeriğiniGetirToolStripMenuItem.Name = "açDosyaSeçipİçeriğiniGetirToolStripMenuItem";
             açDosyaSeçipİçeriğiniGetirToolStripMenuItem.Size = new Size(242, 22);
             açDosyaSeçipİçeriğiniGetirToolStripMenuItem.Text = "Aç… (Dosya seçip içeriğini getir)";
+            açDosyaSeçipİçeriğiniGetirToolStripMenuItem.Click += açDosyaSeçipİçeriğiniGetirToolStripMenuItem_Click;
             // 
             // kaydetAynıDosyayaToolStripMenuItem
             // 
@@ -125,13 +134,6 @@
             görünümToolStripMenuItem.Size = new Size(70, 20);
             görünümToolStripMenuItem.Text = "Görünüm";
             // 
-            // yardımToolStripMenuItem
-            // 
-            yardımToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { hakkındaUygulamaBilgisiToolStripMenuItem });
-            yardımToolStripMenuItem.Name = "yardımToolStripMenuItem";
-            yardımToolStripMenuItem.Size = new Size(56, 20);
-            yardımToolStripMenuItem.Text = "Yardım";
-            // 
             // yazıTipiSeçToolStripMenuItem
             // 
             yazıTipiSeçToolStripMenuItem.Name = "yazıTipiSeçToolStripMenuItem";
@@ -144,23 +146,78 @@
             temaDeğiştirAçıkKoyuToolStripMenuItem.Size = new Size(212, 22);
             temaDeğiştirAçıkKoyuToolStripMenuItem.Text = "Tema Değiştir (Açık/Koyu)";
             // 
+            // yardımToolStripMenuItem
+            // 
+            yardımToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { hakkındaUygulamaBilgisiToolStripMenuItem });
+            yardımToolStripMenuItem.Name = "yardımToolStripMenuItem";
+            yardımToolStripMenuItem.Size = new Size(56, 20);
+            yardımToolStripMenuItem.Text = "Yardım";
+            // 
             // hakkındaUygulamaBilgisiToolStripMenuItem
             // 
             hakkındaUygulamaBilgisiToolStripMenuItem.Name = "hakkındaUygulamaBilgisiToolStripMenuItem";
             hakkındaUygulamaBilgisiToolStripMenuItem.Size = new Size(223, 22);
             hakkındaUygulamaBilgisiToolStripMenuItem.Text = "Hakkında (Uygulama bilgisi)";
             // 
+            // richTextBox1
+            // 
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Location = new Point(0, 24);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(794, 590);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripStatusLabel2, toolStripStatusLabel3, toolStripStatusLabel4 });
+            statusStrip1.Location = new Point(0, 592);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.RenderMode = ToolStripRenderMode.Professional;
+            statusStrip1.Size = new Size(794, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(118, 17);
+            toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(118, 17);
+            toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // toolStripStatusLabel3
+            // 
+            toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            toolStripStatusLabel3.Size = new Size(118, 17);
+            toolStripStatusLabel3.Text = "toolStripStatusLabel3";
+            // 
+            // toolStripStatusLabel4
+            // 
+            toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            toolStripStatusLabel4.Size = new Size(118, 17);
+            toolStripStatusLabel4.Text = "toolStripStatusLabel4";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(794, 614);
+            Controls.Add(statusStrip1);
+            Controls.Add(richTextBox1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Form1";
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -183,5 +240,11 @@
         private ToolStripMenuItem yazıTipiSeçToolStripMenuItem;
         private ToolStripMenuItem temaDeğiştirAçıkKoyuToolStripMenuItem;
         private ToolStripMenuItem hakkındaUygulamaBilgisiToolStripMenuItem;
+        private RichTextBox richTextBox1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripStatusLabel toolStripStatusLabel2;
+        private ToolStripStatusLabel toolStripStatusLabel3;
+        private ToolStripStatusLabel toolStripStatusLabel4;
     }
 }
--- Not Defterim/Form1.cs
+++ Not Defterim/Form1.cs
@@ -1,3 +1,11 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+
+
+
+
 namespace Not_Defterim
 {
     public partial class Form1 : Form
@@ -5,6 +13,30 @@ namespace Not_Defterim
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void yeniYeniSayfaAçToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void açDosyaSeçipÝçeriðiniGetirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Metin Dosyalarý|*.txt|Tüm Dosyalar|*.*";
+            ofd.Title = "Bir dosya seçin";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string dosyaYolu = ofd.FileName;
+                string icerik = File.ReadAllText(dosyaYolu);
+                richTextBox1.Text = icerik;
+            }
         }
     }
 
