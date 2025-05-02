01dbbe602f96eb73584c3be06724c6a7566af8ee
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2025-05-02 21:03:14 +0300

    Proje dosyası ekle.

---
+++ Not Defterim/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Not_Defterim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            dosyaToolStripMenuItem = new ToolStripMenuItem();
+            yeniYeniSayfaAçToolStripMenuItem = new ToolStripMenuItem();
+            açDosyaSeçipİçeriğiniGetirToolStripMenuItem = new ToolStripMenuItem();
+            kaydetAynıDosyayaToolStripMenuItem = new ToolStripMenuItem();
+            farklıKaydetToolStripMenuItem = new ToolStripMenuItem();
+            çıkışToolStripMenuItem = new ToolStripMenuItem();
+            düzenToolStripMenuItem = new ToolStripMenuItem();
+            geriAlToolStripMenuItem = new ToolStripMenuItem();
+            kesKopyalaYapıştırToolStripMenuItem = new ToolStripMenuItem();
+            tümünüSeçToolStripMenuItem = new ToolStripMenuItem();
+            görünümToolStripMenuItem = new ToolStripMenuItem();
+            yardımToolStripMenuItem = new ToolStripMenuItem();
+            yazıTipiSeçToolStripMenuItem = new ToolStripMenuItem();
+            temaDeğiştirAçıkKoyuToolStripMenuItem = new ToolStripMenuItem();
+            hakkındaUygulamaBilgisiToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { dosyaToolStripMenuItem, düzenToolStripMenuItem, görünümToolStripMenuItem, yardımToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(794, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            dosyaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { yeniYeniSayfaAçToolStripMenuItem, açDosyaSeçipİçeriğiniGetirToolStripMenuItem, kaydetAynıDosyayaToolStripMenuItem, farklıKaydetToolStripMenuItem, çıkışToolStripMenuItem });
+            dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            dosyaToolStripMenuItem.Size = new Size(51, 20);
+            dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // yeniYeniSayfaAçToolStripMenuItem
+            // 
+            yeniYeniSayfaAçToolStripMenuItem.Name = "yeniYeniSayfaAçToolStripMenuItem";
+            yeniYeniSayfaAçToolStripMenuItem.Size = new Size(242, 22);
+            yeniYeniSayfaAçToolStripMenuItem.Text = "Yeni (Yeni sayfa aç)";
+            // 
+            // açDosyaSeçipİçeriğiniGetirToolStripMenuItem
+            // 
+            açDosyaSeçipİçeriğiniGetirToolStripMenuItem.Name = "açDosyaSeçipİçeriğiniGetirToolStripMenuItem";
+            açDosyaSeçipİçeriğiniGetirToolStripMenuItem.Size = new Size(242, 22);
+            açDosyaSeçipİçeriğiniGetirToolStripMenuItem.Text = "Aç… (Dosya seçip içeriğini getir)";
+            // 
+            // kaydetAynıDosyayaToolStripMenuItem
+            // 
+            kaydetAynıDosyayaToolStripMenuItem.Name = "kaydetAynıDosyayaToolStripMenuItem";
+            kaydetAynıDosyayaToolStripMenuItem.Size = new Size(242, 22);
+            kaydetAynıDosyayaToolStripMenuItem.Text = "Kaydet (Aynı dosyaya)";
+            // 
+            // farklıKaydetToolStripMenuItem
+            // 
+            farklıKaydetToolStripMenuItem.Name = "farklıKaydetToolStripMenuItem";
+            farklıKaydetToolStripMenuItem.Size = new Size(242, 22);
+            farklıKaydetToolStripMenuItem.Text = "Farklı Kaydet…";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            çıkışToolStripMenuItem.Size = new Size(242, 22);
+            çıkışToolStripMenuItem.Text = "Çıkış";
+            // 
+            // düzenToolStripMenuItem
+            // 
+            düzenToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { geriAlToolStripMenuItem, kesKopyalaYapıştırToolStripMenuItem, tümünüSeçToolStripMenuItem });
+            düzenToolStripMenuItem.Name = "düzenToolStripMenuItem";
+            düzenToolStripMenuItem.Size = new Size(52, 20);
+            düzenToolStripMenuItem.Text = "Düzen";
+            // 
+            // geriAlToolStripMenuItem
+            // 
+            geriAlToolStripMenuItem.Name = "geriAlToolStripMenuItem";
+            geriAlToolStripMenuItem.Size = new Size(194, 22);
+            geriAlToolStripMenuItem.Text = "Geri Al";
+            // 
+            // kesKopyalaYapıştırToolStripMenuItem
+            // 
+            kesKopyalaYapıştırToolStripMenuItem.Name = "kesKopyalaYapıştırToolStripMenuItem";
+            kesKopyalaYapıştırToolStripMenuItem.Size = new Size(194, 22);
+            kesKopyalaYapıştırToolStripMenuItem.Text = "Kes / Kopyala / Yapıştır";
+            // 
+            // tümünüSeçToolStripMenuItem
+            // 
+            tümünüSeçToolStripMenuItem.Name = "tümünüSeçToolStripMenuItem";
+            tümünüSeçToolStripMenuItem.Size = new Size(194, 22);
+            tümünüSeçToolStripMenuItem.Text = "Tümünü Seç";
+            // 
+            // görünümToolStripMenuItem
+            // 
+            görünümToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { yazıTipiSeçToolStripMenuItem, temaDeğiştirAçıkKoyuToolStripMenuItem });
+            görünümToolStripMenuItem.Name = "görünümToolStripMenuItem";
+            görünümToolStripMenuItem.Size = new Size(70, 20);
+            görünümToolStripMenuItem.Text = "Görünüm";
+            // 
+            // yardımToolStripMenuItem
+            // 
+            yardımToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { hakkındaUygulamaBilgisiToolStripMenuItem });
+            yardımToolStripMenuItem.Name = "yardımToolStripMenuItem";
+            yardımToolStripMenuItem.Size = new Size(56, 20);
+            yardımToolStripMenuItem.Text = "Yardım";
+            // 
+            // yazıTipiSeçToolStripMenuItem
+            // 
+            yazıTipiSeçToolStripMenuItem.Name = "yazıTipiSeçToolStripMenuItem";
+            yazıTipiSeçToolStripMenuItem.Size = new Size(212, 22);
+            yazıTipiSeçToolStripMenuItem.Text = "Yazı Tipi Seç…";
+            // 
+            // temaDeğiştirAçıkKoyuToolStripMenuItem
+            // 
+            temaDeğiştirAçıkKoyuToolStripMenuItem.Name = "temaDeğiştirAçıkKoyuToolStripMenuItem";
+            temaDeğiştirAçıkKoyuToolStripMenuItem.Size = new Size(212, 22);
+            temaDeğiştirAçıkKoyuToolStripMenuItem.Text = "Tema Değiştir (Açık/Koyu)";
+            // 
+            // hakkındaUygulamaBilgisiToolStripMenuItem
+            // 
+            hakkındaUygulamaBilgisiToolStripMenuItem.Name = "hakkındaUygulamaBilgisiToolStripMenuItem";
+            hakkındaUygulamaBilgisiToolStripMenuItem.Size = new Size(223, 22);
+            hakkındaUygulamaBilgisiToolStripMenuItem.Text = "Hakkında (Uygulama bilgisi)";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(794, 614);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem dosyaToolStripMenuItem;
+        private ToolStripMenuItem yeniYeniSayfaAçToolStripMenuItem;
+        private ToolStripMenuItem açDosyaSeçipİçeriğiniGetirToolStripMenuItem;
+        private ToolStripMenuItem kaydetAynıDosyayaToolStripMenuItem;
+        private ToolStripMenuItem farklıKaydetToolStripMenuItem;
+        private ToolStripMenuItem çıkışToolStripMenuItem;
+        private ToolStripMenuItem düzenToolStripMenuItem;
+        private ToolStripMenuItem geriAlToolStripMenuItem;
+        private ToolStripMenuItem kesKopyalaYapıştırToolStripMenuItem;
+        private ToolStripMenuItem tümünüSeçToolStripMenuItem;
+        private ToolStripMenuItem görünümToolStripMenuItem;
+        private ToolStripMenuItem yardımToolStripMenuItem;
+        private ToolStripMenuItem yazıTipiSeçToolStripMenuItem;
+        private ToolStripMenuItem temaDeğiştirAçıkKoyuToolStripMenuItem;
+        private ToolStripMenuItem hakkındaUygulamaBilgisiToolStripMenuItem;
+    }
+}
---
+++ Not Defterim/Form1.cs
@@ -0,0 +1,12 @@
+namespace Not_Defterim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+}
